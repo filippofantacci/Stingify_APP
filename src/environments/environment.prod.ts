f534eae0306b657d0ed9c822e8bdcadcf6ced5f2
filippofantacci <56551431+filippofantacci@users.noreply.github.com>
2022-11-23 19:37:43 +0100

    fix per prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { menu } from "src/app/utils/menu";
 export const environment = {
   production: true,
   keycloak: {
-    url: 'http://keycloak:8080/auth',
+    url: 'http://3.238.217.123:8080/auth',
     realm: 'Stingify',
     clientId: 'stingify_app',
     initOptions: {
